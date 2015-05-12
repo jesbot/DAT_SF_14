b0054d6807ec4ec2cb21cb7fef28aec8634d6e25
jesbot <jose.e.solomon@gmail.com>
2015-05-11 22:17:29 -0700

    Started writing my own k-means

---
+++ homework/MyWork/HomeWork2/kMeansDriver.py
@@ -0,0 +1,20 @@
+# K-Means Main driver.
+#
+# Purupose: Applies a basic K-means on the tulip data set.
+#
+# Author: Jose Solomon
+#import numpy as np
+#import matplotlib.pyplot as plt
+#from sklearn.datasets import load_iris
+
+
+# Define the range of nearest neighbors
+nNeighbors = range(1,10,1)
+print nNeighbors
+
+# read the tupil data direclty
+# def kMeansDriver(dataset, k):
+
+# Call the file by name
+# if __name__ == "__main__"
+# 	main()
